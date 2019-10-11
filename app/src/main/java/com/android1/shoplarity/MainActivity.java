a6748ef3d2ed57e1eec20c262e2466673fa7fa9a
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-10-11 19:09:55 +0200

    i added a searchview

--- app/src/main/java/com/android1/shoplarity/MainActivity.java
+++ app/src/main/java/com/android1/shoplarity/MainActivity.java
@@ -2,6 +2,7 @@ package com.android1.shoplarity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.GridView;
@@ -10,14 +11,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     GridView imageGrid;
-    ArrayList category=new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageGrid=(GridView)findViewById(R.id.grid1);
-
         categoryAdapter imgadapter = new categoryAdapter(this);
         imageGrid.setAdapter(imgadapter);
-    }
+}
 }
