ea9f0f86cca3f8f66f6555a58323376d4e0e18a2
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-10-20 15:14:23 +0200

    i added cars

--- app/src/main/java/com/android1/shoplarity/Adapters/carsAdapter.java
+++ app/src/main/java/com/android1/shoplarity/Adapters/carsAdapter.java
@@ -1,26 +1,52 @@
 package com.android1.shoplarity.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-public class carsAdapter extends ArrayAdapter {
+import com.android1.shoplarity.R;
+
+public class carsAdapter extends BaseAdapter {
     Context context;
     private String[] cars;
 
-    public carsAdapter(@NonNull Context context, int resource, String[] cars) {
-        super(context, resource);
+
+    public carsAdapter(Context context, String[] cars) {
         this.context = context;
         this.cars = cars;
     }
+
     @Override
-    public Object getItem(int position){
-        String car=cars[position];
-        return String.format(car);
-    }
-    @Override
-    public int getCount(){
+    public int getCount() {
         return cars.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return cars[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View car_view=convertView;
+
+        if(car_view==null){
+            LayoutInflater insert=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            car_view=insert.inflate(R.layout.carslist,null);
+        }
+        TextView text2=(TextView)car_view.findViewById(R.id.cars1);
+        text2.setText(cars[position]);
+        return car_view;
     }
 }
